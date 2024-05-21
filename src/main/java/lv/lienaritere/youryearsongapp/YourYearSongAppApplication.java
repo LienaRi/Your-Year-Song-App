1e8a415994ad4caf41fbf67eeda46967028ea082
LienaRi <liena.ritere@gmail.com>
2024-05-21 15:45:43 +0300

    First commit

---
+++ src/main/java/lv/lienaritere/youryearsongapp/YourYearSongAppApplication.java
@@ -0,0 +1,13 @@
+package lv.lienaritere.youryearsongapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourYearSongAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourYearSongAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lv/lienaritere/youryearsongapp/YourYearSongAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.lienaritere.youryearsongapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourYearSongAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
